7760b951560e34f85a42f0166d34874c7606b308
ajain <ajain@zipari.com>
2020-05-22 02:08:10 -0400

    button added

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  let [state, setState] = useState({
+    isFocused: false
+  });
+
+  const onFocusChange = () => {
+    setState({
+      isFocused: true
+    });
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        style={(state.isFocused) ? styles.buttonHovered : styles.button}>
+          <Text
+            onFocus = {onFocusChange}
+            style={(state.isFocused) ? styles.buttonTextHovered : styles.buttonText}>
+              UPLOAD IMAGE
+          </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +29,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  button: {
+    backgroundColor: '#404040',
+    padding: 20,
+    borderRadius: 6
+  },
+  buttonHovered: {
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    letterSpacing: 3,
+    shadowRadius: 10,
+    shadowColor: '#000000',
+    shadowOpacity: 0.5
+  },
+  butonText: {
+    color: '#ffffff',
+    fontWeight: '300',
+  },
+  buttonTextHovered: {
+    color: '#404040',
+    fontWeight: '800'
+  }
 });
